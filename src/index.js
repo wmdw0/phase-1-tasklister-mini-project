74a4afa41a8934aed9bac8c5c8c10059bbe5198b
William Walsh <wmdwalsh0@gmail.com>
2021-07-09 11:16:12 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskList = new TaskList();
+
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority");
+
+  const taskUI = document.getElementById("tasks");
+
+  const renderApp = () => (taskUI.innerHTML = taskList.renderTasks());
+
+  newTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    taskList.createNewTask(newTaskDescription.value);
+    e.target.reset();
+    renderApp();
+  });
+
+  taskUI.addEventListener("click", (e) => {
+    if (e.target.nodeName === "BUTTON") {
+      taskList.deleteTask(e.target.dataset.description);
+      renderApp();
+    }
+  });
 });
